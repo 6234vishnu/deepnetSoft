725d3abda09e7450476b12578b47488387dad654
vishnu anil <vishnuanil216@gmail.com>
2025-05-16 08:44:03 +0530

    cors edited

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,34 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT||5000;
+console.log(process.env.FRONTEND_URL);
 
 const frontendUrl = process.env.FRONTEND_URL;
+const allowedOrigins = [
+  frontendUrl
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: frontendUrl,
-    methods: "GET,POST,PUT,PATCH,OPTIONS",
-    allowedHeaders: "Content-Type, Authorization",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: frontendUrl,
+//     methods: "GET,POST,PUT,PATCH,OPTIONS",
+//     allowedHeaders: "Content-Type, Authorization",
+//     credentials: true,
+//   })
+// );
 connectDB()
 
 app.get("/", (req, res) => {
